ec1a8ce1bc5d47e8c399d46a05372c1e59532d1e
rhonall <81948172+rhonall@users.noreply.github.com>
2021-06-17 16:23:42 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const text = document.querySelector('.text')
 const circle = document.querySelector('.circle')
+const playBtn = document.querySelector('.fa-play')
+const pauseBtn = document.querySelector('.fa-pause')
+const bgMusic = document.querySelector('#bgMusic')
 
 const totalTime = 14000
 const breatheTime = (totalTime / 7) * 2.5
 const holdTime = (totalTime / 7)
 
 breatheAnimation ()
+playBtn.addEventListener('click', playMusic)
+pauseBtn.addEventListener('click', pauseMusic)
 
 function breatheAnimation () {
     text.innerHTML = 'Inhale'
@@ -31,4 +36,13 @@ function breatheAnimation () {
 }
 
 setInterval(breatheAnimation, totalTime)
+
+function playMusic() {
+    bgMusic.play()
+}
+
+function pauseMusic() {
+    bgMusic.pause()
+}
+
 
